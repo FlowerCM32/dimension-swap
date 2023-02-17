cb853895b2d501a1b49c447df2e22a7c4d18e50f
FlowerCM32 <104540415+FlowerCM32@users.noreply.github.com>
2023-02-16 20:58:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,44 +8,48 @@ input.onButtonPressed(Button.B, function () {
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (D == 1) {
-        cwall.delete()
-        cland.delete()
+        cwall = game.createSprite(3, 3)
+        cland = game.createSprite(1, 4)
         D = 2
         return
     }
     if (D == 2) {
-        cwall = game.createSprite(3, 3)
-        cland = game.createSprite(1, 4)
+        cwall.delete()
+        cland.delete()
         D = 1
         return
     }
 })
 let w = 0
 let D = 0
+let cwall: game.LedSprite = null
+let cland: game.LedSprite = null
 let man_life: game.LedSprite = null
 let man: game.LedSprite = null
-let cland: game.LedSprite = null
-let cwall: game.LedSprite = null
-cwall = game.createSprite(3, 0)
-cwall = game.createSprite(3, 1)
-cwall = game.createSprite(3, 2)
-cwall = game.createSprite(3, 3)
+let wall = game.createSprite(3, 0)
+wall = game.createSprite(3, 1)
+wall = game.createSprite(3, 2)
 let land = game.createSprite(0, 4)
 let land_1 = game.createSprite(2, 4)
 let land_2 = game.createSprite(3, 4)
 let land_3 = game.createSprite(4, 4)
-cland = game.createSprite(1, 4)
 man = game.createSprite(0, 3)
 man_life = game.createSprite(0, 4)
+cland = game.createSprite(0, 4)
+cwall = game.createSprite(0, 4)
+cland.delete()
+cwall.delete()
 D = 1
 basic.forever(function () {
     if (!(w == 1)) {
         if (!(man_life.isTouching(land) || (man_life.isTouching(land_1) || (man_life.isTouching(land_2) || (man_life.isTouching(land_3) || man_life.isTouching(cland)))))) {
             let sprite: game.LedSprite = null
             man_life.delete()
-            basic.pause(200)
+            basic.pause(500)
+            man.change(LedSpriteProperty.Y, 1)
+            basic.pause(500)
             man.delete()
-            basic.pause(200)
+            basic.pause(500)
             game.gameOver()
             sprite.change(LedSpriteProperty.Y, 1)
         }
